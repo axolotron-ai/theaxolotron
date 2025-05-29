627aae7e05718f1a8b6c9093e62592487e657ffd
Giridharan7193 <giridharan7193gs@gmail.com>
2025-05-29 14:58:29 +0530

    Team Update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,16 +7,24 @@ const lato = Lato({
     variable: '--font-lato',
 });
 
-
 export default function Team() {
     return (
         <>
             <div className={`bg-[#151515] h-screen grid grid-cols-2 items-center justify-center ${lato.className} px-[clamp(1.5rem,7vw,11rem)]`}>
                 <div className="relative flex items-center justify-center">
-                    <Image src="/team.jpg" alt="/reload" height={500} width={500} quality={100} />
-                    <div className="absolute" />
+                    <Image src="/team.jpg" alt="/reload" height={400} width={500} quality={100} className="rounded-[7px] h-[400px] w-[500px] object-cover" />
+                    <div className="absolute inset-0 z-20">
+                        <div className="absolute top-0 left-0 h-[68%] w-[52%] border-2" />
+                        <div className="absolute bottom-0 left-0 h-[30%] w-[52%] border-2" />
+                        <div className="absolute bottom-0 right-0 h-[80%] w-[47%] border-2" />
+                    </div>
+                    <div className="absolute inset-0 z-30">
+                        <div className="absolute top-[68%] left-0 h-[2%] w-[52%] border-2 bg-[#151515]" />
+                        <div className="absolute top-0 right-0 h-[20%] w-[48%] border-2 bg-[#151515]" />
+                        <div className="absolute top-0 left-[52%] h-[100%] w-[1%] border-2 bg-[#151515]" />
+                    </div>
                 </div>
-                <div>
+                <div className="p-[30px]">
                     <p className="text-white font-semibold text-[clamp(1rem,4vw,4rem)]">THE TEAM.</p>
                     <p className="text-white/70 text-[clamp(0.8rem,1.3vw,2rem)]">At Axolotron, we help ambitious businesses unlock the real power of AI. Whether you're cutting costs, streamlining operations, or scaling smarter—we build solutions that deliver results. No fluff, just data-driven impact. Partner with us to turn intelligence into action, and ideas into measurable growth.</p>
                 </div>
