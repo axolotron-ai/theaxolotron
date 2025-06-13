bb5e697164c596626ab156743a728b4cfc1ad3ab
Giridharan7193 <giridharan7193gs@gmail.com>
2025-06-13 17:42:29 +0530

    Update Commit

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyDk0ptyKAnYtbFpcIHDY3a04fyYdt38hF0",
+    authDomain: "axolotron-120a1.firebaseapp.com",
+    projectId: "axolotron-120a1",
+    storageBucket: "axolotron-120a1.appspot.com",
+    messagingSenderId: "948141955254",
+    appId: "1:948141955254:web:fab5c3115e4ab29c9aae17",
 };
 
 const app = initializeApp(firebaseConfig);
