69d49826a7ef3e5ce81407bcb0fc6502345f2b54
Giridharan7193 <giridharan7193gs@gmail.com>
2025-06-22 23:18:26 +0530

    Update 3.12

--- src/components/academy/Vision.jsx
+++ src/components/academy/Vision.jsx
@@ -6,7 +6,7 @@ export default function Vision() {
             <div className="grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 py-[50px] px-[30px] sm:py-[130px] sm:px-[100px] bg-white rounded-t-[70px]">
                 <div className="flex flex-col gap-[25px] mb-[15px]">
                     <p className="text-[40px] font-semibold">Company's Vision</p>
-                    <p className="font-semibold sm:pr-[80px] text-[20px]">To make world-class AI education accessible, practical, and transformative — so every learner is not just trained, but truly prepared for the future of work. <br /><span>We don't create certificate holders. We build confident, job-ready AI professionals.</span></p>
+                    <p className="font-semibold sm:pr-[80px] text-[20px]">To make world-class AI education accessible, practical, and transformative — so every learner is not just trained, but truly prepared for the future of work. <br />We don't create certificate holders. We build confident, job-ready AI professionals.</p>
                     <button className="hidden sm:block outline-1 outline-black cursor-pointer w-fit px-[20px] py-[10px] rounded-[7px] font-semibold text-[18px] mt-[40px]">View Gallery</button>
                 </div>
                 <div>
