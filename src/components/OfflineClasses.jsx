a6fc7a50abb24663438752515dfbf8f2172f9f62
Giridharan7193 <giridharan7193gs@gmail.com>
2025-06-22 17:07:38 +0530

    Update 3.8

--- src/components/OfflineClasses.jsx
+++ src/components/OfflineClasses.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { getData } from "@/firebase/firestoreService";
 
 export default function OfflineClasses() {
 
