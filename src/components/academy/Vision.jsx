8daee27139bf6359a6e207c5a5d62044f298ae83
Giridharan7193 <giridharan7193gs@gmail.com>
2025-06-25 22:39:28 +0530

    Update 6.1

--- src/components/academy/Vision.jsx
+++ src/components/academy/Vision.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 
 export default function Vision() {
@@ -7,7 +8,9 @@ export default function Vision() {
                 <div className="flex flex-col gap-[25px] mb-[15px]">
                     <p className="text-[40px] font-semibold">Company's Vision</p>
                     <p className="font-semibold sm:pr-[80px] text-[20px]">To make world-class AI education accessible, practical, and transformative — so every learner is not just trained, but truly prepared for the future of work. <br />We don't create certificate holders. We build confident, job-ready AI professionals.</p>
-                    <button className="hidden sm:block outline-1 outline-black cursor-pointer w-fit px-[20px] py-[10px] rounded-[7px] font-semibold text-[18px] mt-[40px]">View Gallery</button>
+                    <button
+                        onClick={() => document.getElementById("gallery").scrollIntoView({ behavior: "smooth" })}
+                        className="hidden sm:block outline-1 outline-black cursor-pointer w-fit px-[20px] py-[10px] rounded-[7px] font-semibold text-[18px] mt-[40px]">View Gallery</button>
                 </div>
                 <div>
                     <Image src="/vision.jpg" alt="/reload" height={200} width={200} quality={100} className="h-[350px] w-[550px] rounded-[12px]" />
