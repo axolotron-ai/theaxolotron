e3893c48f619a9dcb50ed2234585bffc1bbc8f21
Giridharan7193 <giridharan7193gs@gmail.com>
2025-06-26 17:39:49 +0530

    update 5.5

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.jpg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
